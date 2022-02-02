717908be2b2960432ca3dde9f0b906c722b19afa
David R Connell <davidconnell12@gmail.com>
2022-02-01 19:51:48 -0600

    Init: setup package

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "R package to parse pubmed xml files into a set of graphes.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.rWrapper.override {
+              packages = with pkgs.rPackages;
+                [ tidyverse lintr styler xml2 devtools ];
+            })
+          ];
+        };
+      });
+}
