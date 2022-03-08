5f86a060634879434d6f6d80c7511b2f5556acf0
David R Connell <davidconnell12@gmail.com>
2022-03-08 00:09:38 -0600

    Add: common test script
    It got missed since before since I forgot to whitelist it in
    .gitignore.

---
+++ tests/bats_helpers.bash
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+_common_file_setup() {
+    export BATS_DIR=$(dirname $BATS_TEST_FILENAME)
+    export PATH="$BATS_DIR/../bin:$PATH"
+    export cache_dir=$BATS_DIR/cache
+    export structure_file=$BATS_DIR/data/test_xml_reader_structure.yml
+    export OMP_NUM_THREADS=4
+    mkdir -p $cache_dir
+}
+
+_common_setup() {
+    cd $BATS_DIR
+}
+
+_common_file_teardown() {
+    rm -r $cache_dir
+}
