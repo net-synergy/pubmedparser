469839a9e6c9712aae7290a0b086acfd43f90c78
David R Connell <davidconnell12@gmail.com>
2022-04-22 14:26:41 -0500

    Optimize: reduce comparisons by using symmetry of the overlap matrix
    Add in a check to ensure the primary column is sorted. (Needed for
    using symmetry but also needed so that the last line of the contains
    the largest ID anyway.) If the primary column is sorted, only need to
    check lines below it since overlap matrix is
    symmetric (`overlap[smaller][larger] == overlap[larger][smaller]`) and
    only the first case is printed.
    
    Might be useful to keep track of the diagonal values for normalizing
    but that can be added in later if needed easily.
    
    Also did some cleaning by reorder the code removing the need to rewind
    and skip lines in a few places.

--- src/overlap.c
+++ src/overlap.c
@@ -1,3 +1,16 @@
+/* overlap -- calculate the overlap between nodes
+   Overlap is the intersect between the values of two nodes. Expects a
+   table with two columns of IDs. Each column represents a node of a
+   single type and each row indicates a graph edge.
+
+   So the overlap is the number of common nodes in a bipartisian graph
+   between nodes of one type.
+
+   Requires the primary nodes column to be sorted in order to find
+   highest ID and to reduce number of calculations (take advantage of
+   symmetry).
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,28 +36,19 @@ int get_n_nodes(FILE *fptr)
 
 }
 
-int fgetl(FILE *fptr)
-{
-  char c = EOF;
-  while (c != '\n') {
-    c = getc(fptr);
-  }
-  return EXIT_SUCCESS;
-}
-
 int main(int argc, char **argv)
 {
   char *f = argv[argc - 1];
 
-  FILE *fptr_1, *fptr_2;
-  if (!(fptr_1 = fopen(f, "r")) || !(fptr_2 = fopen(f, "r"))) {
+  FILE *fptr;
+  if (!(fptr = fopen(f, "r"))) {
     fprintf(stderr, "Error: could not open file %s\n", f);
     return EXIT_FAILURE;
   }
 
   int n_nodes = 0;
-  if ((n_nodes = get_n_nodes(fptr_1)) == -1) {
-    fprintf(stderr, "Error: %s.\n%s.",
+  if ((n_nodes = get_n_nodes(fptr)) == -1) {
+    fprintf(stderr, "Error: %s.\n%s.\n",
 	    "could not get number of nodes",
 	    "Last line of input file should have two columns of IDs.");
     return EXIT_FAILURE;
@@ -57,24 +61,27 @@ int main(int argc, char **argv)
     }
   }
 
+  char header[2][100];
+  if (fscanf(fptr, "%s\t%s", header[0], header[1]) != 2) {
+    fprintf(stderr, "Error: could not read header\n");
+    return EXIT_FAILURE;
+  }
+
+  int current_line = 0;
   int primary[] = { 0, 0 };
   int secondary[] = { 0, 0 };
-  fgetl(fptr_1); // Skip header
-  while (fscanf(fptr_1, "%d\t%d", &primary[0], &secondary[0]) == 2) {
-    rewind(fptr_2);
-    fgetl(fptr_2);
-    while (fscanf(fptr_2, "%d\t%d", &primary[1], &secondary[1]) == 2) {
+  while (fscanf(fptr, "%d\t%d", &primary[0], &secondary[0]) == 2) {
+    current_line = ftell(fptr);
+    while (fscanf(fptr, "%d\t%d", &primary[1], &secondary[1]) == 2) {
       if (secondary[0] == secondary[1]) {
+	if (primary[0] > primary[1]) {
+	  fprintf(stderr, "Error: primary column is not sorted.\n");
+	  return EXIT_FAILURE;
+	}
         overlap[primary[0] - 1][primary[1] - 1]++;
       }
     }
-  }
-
-  char header[2][100];
-  rewind(fptr_1);
-  if (fscanf(fptr_1, "%s\t%s", header[0], header[1]) != 2) {
-    fprintf(stderr, "Error: could not read header");
-    return EXIT_FAILURE;
+    fseek(fptr, current_line, SEEK_SET);
   }
 
   printf("%s\t%s\tweight\n", header[0], header[0]);
@@ -86,8 +93,7 @@ int main(int argc, char **argv)
     }
   }
 
-  fclose(fptr_1);
-  fclose(fptr_2);
+  fclose(fptr);
 
   return EXIT_SUCCESS;
 }
--- tests/test_overlap_calculator.bats
+++ tests/test_overlap_calculator.bats
@@ -9,13 +9,25 @@ setup_file() {
     load 'bats_helpers'
     _common_file_setup
 
-    cat <<-EOF > $cache_dir/edges.tsv
+    cat <<-EOF > $cache_dir/normal_edges.tsv
 NODE1 NODE2
 1     1
 1     2
 1     3
 2     1
 2     2
+3     1
+3     3
+4     3
+EOF
+
+    cat <<-EOF > $cache_dir/disordered_edges.tsv
+NODE1 NODE2
+2     1
+2     2
+1     1
+1     2
+1     3
 3     1
 3     3
 4     3
@@ -28,7 +40,7 @@ teardown_file() {
 }
 
 @test "Test overlap calculator" {
-    overlap $cache_dir/edges.tsv > $cache_dir/overlap.tsv
+    overlap $cache_dir/normal_edges.tsv > $cache_dir/overlap.tsv
     diff $cache_dir/overlap.tsv <(cat<<EOF
 NODE1	NODE1	weight
 1	2	2
@@ -38,4 +50,11 @@ NODE1	NODE1	weight
 3	4	1
 EOF
 				 )
+}
+
+@test "Test unsorted primary column causes error" {
+    run overlap $cache_dir/disordered_edges.tsv
+
+    [ "$status" -eq 1 ]
+    [ "$output" = "Error: primary column is not sorted." ]
 }
