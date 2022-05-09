3786b9f424621928e7fc7bce690fc8434361c4ae
David R Connell <davidconnell12@gmail.com>
2022-05-09 12:40:21 -0500

    Fix: use normal input arguments instead of trying to use an array

--- example/calculate_overlap.sh
+++ example/calculate_overlap.sh
@@ -3,20 +3,11 @@
 # Usage: calculate_overlap node1 node2 ...
 # Resulting file names will be generated based on node names.
 
-
-declare -a overlap_nodes
-
-count=0
-while [[ "$#" -gt 0 ]]; do
-    overlap_nodes[count] = "$1"
-    count=(( $count + 1 ));
-    shift
-done
 source ./env.sh
 
 echo "Calculating overlap between publications..."
 
-for node in "${overlap_nodes[@]}"; do
+for node in "$@"; do
     [[ $node == "Reference" ]] && node=$key
     echo -e ":START_ID(${key})\t:END_ID(${key})\tweight" > \
         $import_dir/${key}_${node}_overlap.tsv
