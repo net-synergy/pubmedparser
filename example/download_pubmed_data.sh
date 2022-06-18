7be1d3f18ef674105ba39f2f359570c6b25eae67
David R Connell <davidconnell12@gmail.com>
2022-06-17 23:37:42 -0500

    Add: list files option

--- example/download_pubmed_data.sh
+++ example/download_pubmed_data.sh
@@ -7,6 +7,9 @@
 # Find pubmed files:
 #     ftp://ftp.ncbi.nlm.nih.gov/pubmed/{baseline,updatefiles}
 
+base_url="ftp://ftp.ncbi.nlm.nih.gov/pubmed"
+name_prefix="pubmed22n"
+
 usage() {
 	cat <<-_EOF_
 		Useage: $(basename $0) OPTION... FILE[S]
@@ -16,6 +19,7 @@ usage() {
 
 		  -s    source directory, which pubmed directory to get data from (baseline|updatefiles).
 		  -d    destination directory, where to save files to (defaults to \$PWD).
+		  -l    list files in source directory, if source directory is unset, show both.
 		  -h    Show this help.
 
 		TODO:
@@ -41,7 +45,6 @@ expand_file_names() {
 download_files() {
 	local src_dir="$1"
 	shift
-	local base_url="ftp://ftp.ncbi.nlm.nih.gov/pubmed/${src_dir}"
 	local file_names=$(expand_file_names "${name_prefix}%s.xml.gz" "$@")
 	local file_urls=$(expand_file_names "${base_url}/${src_dir}/%s" "$file_names")
 
@@ -49,19 +52,27 @@ download_files() {
 	wget $(expand_file_names %s.md5 "$file_urls")
 
 	checks=$(md5sum -c *.md5 2>/dev/null)
-	echo -e "${checks}" | grep "OK" | grep -o "pubmed22n[0-9]\+.xml.gz" |
+	echo -e "${checks}" | grep "OK" | grep -o "${name_prefix}[0-9]\+.xml.gz" |
 		xargs -I {} rm {}.md5
 
-	echo -e "${checks}" | grep "FAILED" | grep -o "pubmed22n[0-9]\+.xml.gz" |
+	echo -e "${checks}" | grep "FAILED" | grep -o "${name_prefix}[0-9]\+.xml.gz" |
 		xargs -I {} sh -c "echo {} failed md5sum check, deleting && rm {}* >&2"
+}
+
+list_files() {
+	local src_dir=$1
+	curl --silent "${base_url}/${src_dir}/" |
+		sed -n s/".*${name_prefix}\([0-9]\+\)\.xml\.gz$"/"\1"/p
 }
 
 src_dir="baseline"
 dest_dir=$PWD
-while getopts "d:s:h" arg; do
+list_flag=false
+while getopts "d:s:lh" arg; do
 	case $arg in
 	s) src_dir="$OPTARG" ;;
 	d) dest_dir="$OPTARG" ;;
+	l) list_flag=true ;;
 	h)
 		usage
 		exit 0
@@ -73,6 +84,11 @@ while getopts "d:s:h" arg; do
 	esac
 done
 shift $(($OPTIND - 1))
+
+if $list_flag; then
+	list_files $src_dir
+	exit 0
+fi
 
 if [[ "$#" -eq 0 ]]; then
 	usage >&2
