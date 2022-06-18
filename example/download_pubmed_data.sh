4614ed4497ffb8d363d6b074497e30c97ab51a55
David R Connell <davidconnell12@gmail.com>
2022-06-17 19:45:07 -0500

    Add: source and destination directory options

--- example/download_pubmed_data.sh
+++ example/download_pubmed_data.sh
@@ -9,14 +9,16 @@
 
 usage() {
 	cat <<-_EOF_
-		Useage: $(basename $0) download_pubmed_data.sh OPTION... FILE[S]
+		Useage: $(basename $0) OPTION... FILE[S]
 		Downloads publication data files from pubmed and checks against md5s.
 
-		The FILE argument can be the number of a file or a range of file numbers (i.e. {1..10}).
+		The FILE argument can be the number of a file or a range of file numbers (i.e. {0001..0010}).
+
+		  -s    source directory, which pubmed directory to get data from (baseline|updatefiles).
+		  -d    destination directory, where to save files to (defaults to \$PWD).
+		  -h    Show this help.
 
 		TODO:
-		- Currently only checks the baseline directory, should
-		eventually look in the updatefiles directory.
 		- Should accept baseline/updatefiles/all flags to
 		download all files in either baseline, updatefiles
 		or both directories.
@@ -37,11 +39,11 @@ expand_file_names() {
 }
 
 download_files() {
-	local n_files="$#"
-	local baseline_url="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline"
-	local file_names=$(expand_file_names pubmed22n%s.xml.gz "$@")
-	local file_urls=$(expand_file_names "${baseline_url}/%s" "$file_names")
-	cd $cache_dir
+	local src_dir="$1"
+	shift
+	local base_url="ftp://ftp.ncbi.nlm.nih.gov/pubmed/${src_dir}"
+	local file_names=$(expand_file_names "${name_prefix}%s.xml.gz" "$@")
+	local file_urls=$(expand_file_names "${base_url}/${src_dir}/%s" "$file_names")
 
 	wget $file_urls
 	wget $(expand_file_names %s.md5 "$file_urls")
@@ -54,12 +56,36 @@ download_files() {
 		xargs -I {} sh -c "echo {} failed md5sum check, deleting && rm {}* >&2"
 }
 
-[ -d $data_dir ] || mkdir -p $data_dir
+src_dir="baseline"
+dest_dir=$PWD
+while getopts "d:s:h" arg; do
+	case $arg in
+	s) src_dir="$OPTARG" ;;
+	d) dest_dir="$OPTARG" ;;
+	h)
+		usage
+		exit 0
+		;;
+	?)
+		usage >&2
+		exit 1
+		;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+if [[ "$#" -eq 0 ]]; then
+	usage >&2
+	exit 1
+fi
+
+[ -d $dest_dir ] || mkdir -p $dest_dir
+cd $dest_dir
 
 # Potentially make smarter check to see if the requested files already
 # exist.
-if [ $(ls $data_dir | wc -w) -eq 0 ]; then
+if [ $(ls $PWD | wc -w) -eq 0 ]; then
 	echo "Downloading files..."
-	download_files "$@"
+	download_files $src_dir "$@"
 	echo "Finished downloading files."
 fi
