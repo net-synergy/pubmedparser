19a93ede67f18b22e1e21b9b8fb3d791341f4dc4
David R Connell <davidconnell12@gmail.com>
2023-03-25 22:14:14 -0500

    Delete code that relied on no longer available yaml reader
    Remove the CLI yaml reader since changes to structure broke it and project is
    moving towards using python for calling.

--- example/creating_graphs.sh
+++ example/creating_graphs.sh
@@ -12,15 +12,3 @@ structure_file=example/structure.yml
 
 helpers/download_pubmed_data --destination $xml_dir {0001..0003}
 result/bin/read_xml --structure-file=$structure_file --cache=$cache_dir $xml_dir/*
-
-helpers/convert2graph.sh \
-    --structure-file $structure_file \
-    --source $cache_dir \
-    --destination $import_dir
-
-helpers/generate_importer.sh \
-    --structure-file $structure_file \
-    --import-dir $import_dir >importpubmed.sh
-chmod +x importpubmed.sh
-
-./importpubmed.sh
