e722606748466ed4cd471c306acf7f91b3803271
David R Connell <davidconnell12@gmail.com>
2023-03-27 17:38:47 -0500

    Setup python package

--- flake.nix
+++ flake.nix
@@ -11,12 +11,29 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         src = self;
-        version = "1.1.4";
+        version = "2.0.0-alpha.1";
+        pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          editablePackageSources = { pubmedparser = ./pubmedparser; };
+          preferWheels = true;
+          extraPackages = (ps:
+            with ps; [
+              ipython
+              python-lsp-server
+              pyls-isort
+              python-lsp-black
+              pylsp-mypy
+            ]);
+          groups = [ ];
+        };
+        pythonPackage = pkgs.poetry2nix.mkPoetryPackage { projectDir = ./.; };
       in {
         packages.pubmedparser = pkgs.callPackage ./. { inherit src version; };
+        packages.python = pythonPackage;
         defaultPackage = self.packages.${system}.pubmedparser;
         devShell = pkgs.mkShell {
-          buildInputs = (with pkgs; [ gcc gdb astyle zlib bats cmocka ]);
+          packages =
+            (with pkgs; [ gcc gdb astyle zlib bats cmocka poetry pythonEnv ]);
         };
       });
 }
