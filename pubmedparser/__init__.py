ddcff0b6698da11f6517905bd10a89c9c75c4d5a
David R Connell <davidconnell12@gmail.com>
2023-05-01 20:01:37 -0500

    Add docstring

--- pubmedparser/__init__.py
+++ pubmedparser/__init__.py
@@ -30,7 +30,40 @@ def read_xml(
     n_threads: int = -1,
     exts: tuple[str, ...] = (".xml", ".xml.gz")
 ) -> None:
-    """ """
+    """
+    Collect values matching xpaths in XML files
+
+    Parameters
+    ----------
+    files : str, list
+        A path to the directory containing the paths to read or a list of
+        absolute paths to the files to read.
+    path_structure : dict, str
+        A set of xpaths to collect. Either as a dictionary with keys
+        representing names and values representing the paths or a path to a
+        yaml file describing the same information.
+    cache_dir : str
+        Where to save the results.
+    relative_to_default_cache : bool
+        If true, `cache_dir` is a subdirectory under pubmedparser's default
+        cache directory. If false, `cache_dir` is relative to the cwd.
+    progress_file : str, default "processed.txt"
+        If not none, save successfully parsed files to the progress file. If
+        the file already exists, only files not listed in it will be read.
+    n_threads : int, default -1
+        Number of files to process in parallel. If -1, use system default.
+    exts : tuple, default (".xml", ".xml.gz")
+        A tuple of file extensions to include. Any file ending with an
+        extension not included will be ignored.
+    """
+
+    if isinstance(files, str):
+        if os.path.isdir(files):
+            files = os.listdir(files)
+        elif os.path.exists(files):
+            files = [files]
+        else:
+            raise FileNotFoundError("Files path not found")
 
     if relative_to_default_cache:
         cache_dir = default_cache_dir(cache_dir)
