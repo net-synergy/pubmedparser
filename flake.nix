c52c25ab02edfde1a7f1a9bf36291f56c7a98e04
David R Connell <david32@dcon.addy.io>
2024-11-04 19:40:58 -0600

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -12,18 +12,15 @@
   outputs = { self, nixpkgs, poetry2nix }:
     let
       system = "x86_64-linux";
-      python = pkgs.python311;
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ poetry2nix.overlays.default ];
       };
 
       pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
-        inherit python;
         projectDir = ./.;
         editablePackageSources = { pubmedparser = ./.; };
         preferWheels = true;
-        groups = [ "dev" ];
       };
     in {
       devShells.${system}.default =
